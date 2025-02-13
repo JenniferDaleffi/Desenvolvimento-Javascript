48bf0dd7ae205e61bff06aacd8751d4be2490e95
JenniferDaleffi <jenniferdaleffii@gmail.com>
2025-02-13 19:35:09 -0300

    var and constant

---
+++ Exercicios Javascript/constants.js
@@ -0,0 +1,12 @@
+// atribuindo valor para const
+
+const number  = 10;
+const objeto = {
+    nombre: 'Juan'
+    apellido: 'Perez'
+}
+
+objeto.nombre = 'Pedro';
+objeto.apellido = 'Gomez';
+console.log(objeto.nome);
+
---
+++ Exercicios Javascript/var.js
@@ -0,0 +1,9 @@
+function varExample () {
+    for(var i = 0; i < 3; i++){
+        setTimeout(function () {
+            console.log(i);
+        }, 1000)
+    }
+}
+
+varExample();
